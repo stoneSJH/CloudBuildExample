7a152e2ccbaafa504f4922a53b69688087dd7330
stone <shijiahao@unity3d.com>
2018-07-13 11:52:28 +0800

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetButton("SwitchButton").onClick.AddListener(() =>
+		{
+			Application.LoadLevel("1");
+		});
+	}
+	
+	private Button GetButton(string buttonName)
+	{
+		GameObject obj = GameObject.Find(buttonName);
+		if (obj != null)
+		{
+			return obj.GetComponent<Button>();
+		}
+		return null;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
